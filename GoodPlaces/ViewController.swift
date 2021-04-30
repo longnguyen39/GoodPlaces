fb3d41482d095f40c0a759c6684e794e730d2600
Long Nguyen <elongmusk39@gmail.com>
2021-04-29 20:46:10 -0700

    Initial Commit

---
+++ GoodPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodPlaces
+//
+//  Created by Long Nguyen on 4/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
